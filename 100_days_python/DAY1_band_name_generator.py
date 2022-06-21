5faf8294c78e8bc850ab01c78a4fe591957849c4
Hot6x <95900411+OrcunLee@users.noreply.github.com>
2022-06-21 21:54:09 +0900

    DAY1

---
+++ 100_days_python/DAY1_band_name_generator.py
@@ -0,0 +1,4 @@
+print('Welcom to the band name generator')
+city = input('Which city did you grow up in? : ')
+pet = input ('What is the name of a pet? : ')
+print('Your band name could be ' + city + ' ' + pet)
---
+++ readme.md
@@ -0,0 +1,7 @@
+# practice.
+연습삼아 작성한 코드, 혼자서 푼 알고리즘 문제와 여러가지 모음
+
+## Best 100 days python
+Udemy의 Python 강의 들으면서 연습
+
+## 
